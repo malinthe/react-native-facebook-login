dcbd6ba8cae0079ae9d98f2bbbf9bba9df88a9c6
Malinthe Samarakkon <malinthe@gmail.com>
2016-04-25 18:52:05 +0530

    added custom touchable component directly to android button

--- index.android.js
+++ index.android.js
@@ -1,13 +1,14 @@
 var React = require('react-native');
 var {
-  NativeModules,
-  StyleSheet,
-  View,
-  TouchableHighlight,
-  Text
+    NativeModules,
+    StyleSheet,
+    View,
+    TouchableHighlight,
+    Text
 } = React;
 
 var FBLoginManager = NativeModules.FBLoginManager;
+var YamuTouchable = require('../../modules/YamuTouchable');
 
 var itypeof = function (val) {
     return Object.prototype.toString.call(val).replace(/(\[|object|\s|\])/g, '').toLowerCase();
@@ -15,80 +16,80 @@ var itypeof = function (val) {
 
 
 var FBLogin = React.createClass({
-  getInitialState() {
-   var statics = {
-     loginText: 'Login with Facebook',
-     logoutText: 'Logout from Facebook'
-   };
-  return {
-    statics:statics,
-    isLoggedIn: false,
-    buttonText: statics.loginText
-  };
-},
-  logout() {
-    FBLoginManager.logout((err, data) => this._handleEvent(err, data));
-},
-componentWillMount: function(){
-  var self = this;
-  FBLoginManager.getCurrentToken(function(token){
-    if(itypeof(token) === 'string' && token.length > 0){
-      self.setState({isLoggedIn:true, buttonText: self.state.statics.logoutText});
-    }else{
-      self.setState({isLoggedIn:false, buttonText: self.state.statics.loginText});
-    }
-  })
-},
-  _handleEvent(e, data) {
+    getInitialState() {
+        var statics = {
+            loginText: 'Login with Facebook',
+            logoutText: 'Logout from Facebook'
+        };
+        return {
+            statics: statics,
+            isLoggedIn: false,
+            buttonText: statics.loginText
+        };
+    },
+    logout() {
+        FBLoginManager.logout((err, data) => this._handleEvent(err, data));
+    },
+    componentWillMount: function () {
+        var self = this;
+        FBLoginManager.getCurrentToken(function (token) {
+            if (itypeof(token) === 'string' && token.length > 0) {
+                self.setState({isLoggedIn: true, buttonText: self.state.statics.logoutText});
+            } else {
+                self.setState({isLoggedIn: false, buttonText: self.state.statics.loginText});
+            }
+        })
+    },
+    _handleEvent(e, data) {
 
-    var result = e || data;
-    if(result.type === 'success' && result.profile){
-      try{
-        result.profile = JSON.parse(result.profile)
-      }catch(err){
-        console.warn('Could not parse facebook profile: ', result.profile);
-        console.error(err);
-      }
-    }
+        var result = e || data;
+        if (result.type === 'success' && result.profile) {
+            try {
+                result.profile = JSON.parse(result.profile)
+            } catch (err) {
+                console.warn('Could not parse facebook profile: ', result.profile);
+                console.error(err);
+            }
+        }
 
-    if(result.eventName === 'onLogin' || result.eventName === 'onLoginFound'){
-      this.setState({isLoggedIn:true, buttonText: this.state.statics.logoutText});
-    }else if(result.eventName === 'onLogout'){
-      this.setState({isLoggedIn:false, buttonText: this.state.statics.loginText});
-    }
+        if (result.eventName === 'onLogin' || result.eventName === 'onLoginFound') {
+            this.setState({isLoggedIn: true, buttonText: this.state.statics.logoutText});
+        } else if (result.eventName === 'onLogout') {
+            this.setState({isLoggedIn: false, buttonText: this.state.statics.loginText});
+        }
 
-    if(result.eventName && this.props.hasOwnProperty(result.eventName)){
-      var event = result.eventName;
-      delete result.eventName;
-      console.log('Triggering \'%s\' event', event)
-      this.props[event](result);
-    }else{
-      console.log('\'%s\' Event is not defined or recognized', result.eventName)
-    }
-  },
+        if (result.eventName && this.props.hasOwnProperty(result.eventName)) {
+            var event = result.eventName;
+            delete result.eventName;
+            console.log('Triggering \'%s\' event', event)
+            this.props[event](result);
+        } else {
+            console.log('\'%s\' Event is not defined or recognized', result.eventName)
+        }
+    },
 
-  _onFacebookPress() {
-    var permissions = ['email', 'public_profile'];
-    if( itypeof(this.props.permissions) === 'array'){
-      permissions = this.props.permissions;
-    }
-    
-    if(this.state.isLoggedIn){
-      FBLoginManager.logout((err,data) => this._handleEvent(err,data));
-    }else{
-      FBLoginManager.loginWithPermissions(permissions, (err,data) => this._handleEvent(err,data));
-    }
-   },
+    _onFacebookPress() {
+        var permissions = ['email', 'public_profile'];
+        if (itypeof(this.props.permissions) === 'array') {
+            permissions = this.props.permissions;
+        }
 
-  render: function(){
-    return (
-      <TouchableHighlight onPress={this._onFacebookPress}>
-          <View style={[styles.login, this.props.style]}>
-              <Text style={styles.whiteFont}> {this.state.buttonText} </Text>
-          </View>
-      </TouchableHighlight>
-    )
-  }
+        if (this.state.isLoggedIn) {
+            FBLoginManager.logout((err, data) => this._handleEvent(err, data));
+        } else {
+            FBLoginManager.loginWithPermissions(permissions, (err, data) => this._handleEvent(err, data));
+        }
+    },
+
+    render: function () {
+        return (
+            <YamuTouchable onPress={this._onFacebookPress}>
+                <View style={[styles.login, this.props.style]}>
+                    <Text style={styles.whiteFont}> {this.state.buttonText} </Text>
+                </View>
+            </YamuTouchable>
+        )
+    }
 });
 
 var styles = StyleSheet.create({
